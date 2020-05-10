827b216fe8f6e16db9010209b7e70b92b308352a
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-11 05:10:36 +0600

    Create LIKE.sql

---
+++ SELECT/LIKE.sql
@@ -0,0 +1,8 @@
+SELECT <columns>
+FROM <table>
+WHERE <column> LIKE 'c%'   -- will find a value which starts from "c"
+
+
+SELECT <columns>
+FROM <table>
+WHERE <column> LIKE '%c'   -- will find a value which ends with "c"
