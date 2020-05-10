f2390f6aea3ba0866f825b7581c184ebac0844c3
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-11 05:52:23 +0600

    Update LEFT OUTER JOIN.sql

--- JOINING/LEFT OUTER JOIN.sql
+++ JOINING/LEFT OUTER JOIN.sql
@@ -6,3 +6,13 @@ FROM <table 1>
 LEFT OUTER JOIN <table 2>
 ON <matching column syntax> -- emp.employee_id = order.employee_id
 ORDER BY <table1.column>; -- optional
+
+
+
+-- LEFT OUTER JOIN and LEFT JOIN are same commands
+
+SELCT table1.column, table2.column, . . . .
+FROM <table 1>
+LEFT JOIN <table 2>
+ON <matching column syntax> -- emp.employee_id = order.employee_id
+ORDER BY <table1.column>; -- optional
