3e4fdfd90a3ccc8ac4ba4a4f2e188698df003015
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-11 05:11:54 +0600

    Update LIKE.sql

--- SELECT/LIKE.sql
+++ SELECT/LIKE.sql
@@ -1,8 +1,13 @@
 SELECT <columns>
 FROM <table>
-WHERE <column> LIKE 'c%'   -- will find a value which starts from "c"
+WHERE <column> LIKE 'c%'   -- will find a value which starts with "c"
 
 
 SELECT <columns>
 FROM <table>
 WHERE <column> LIKE '%c'   -- will find a value which ends with "c"
+
+
+SELECT <columns>
+FROM <table>
+WHERE <column> LIKE 'm%r'   -- will find a value which starts with "m" and ends with "r"
