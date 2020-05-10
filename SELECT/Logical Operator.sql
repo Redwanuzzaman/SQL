88604e39a394e9e764c226e4fe7f2c95dc1f6a9a
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-05-11 04:56:29 +0600

    AND, OR, NOT

---
+++ SELECT/Logical Operator.sql
@@ -0,0 +1,16 @@
+--AND
+SELECT <columns>
+FROM <table>
+WHERE <condition 1> AND <condition 2> ...
+
+
+--OR
+SELECT <columns>
+FROM <table>
+WHERE <condition 1> OR <condition 2> ...
+
+
+--NOT
+SELECT <columns>
+FROM <table>
+WHERE <column> NOT IN (<unwanted values>);
